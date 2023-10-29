cb8b56e74b6df046daefdb0b71c6d632479fa846
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2023-10-29 13:20:31 -0600

    V3 - Funciona carga modelov3 - Falta desarrollo

--- app.py
+++ app.py
@@ -12,47 +12,43 @@ from PIL import Image
 
 predictor = joblib.load("mosquitos_v3.sav")
 
-# Título de la aplicación
-st.title('Mosquitos')
-
 # Sidebar for navigation
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", ["Make Predictions", "Model Performance", "Data Information"])
+st.sidebar.title("Opciones")
+selection = st.sidebar.radio("Ir a", ["Realizar predicciones", "Performance de modelos", "Información extra"])
 
 # Main content
-if selection == "Make Predictions":
-    st.title("Model Predictions")
+if selection == "Realizar predicciones":
+    st.title("Predicciones")
 
     # Upload an image for prediction
-    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+    uploaded_image = st.file_uploader("Cargar imagen", type=["jpg", "png", "jpeg"])
     if uploaded_image is not None:
-        st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+        st.image(uploaded_image, caption="Imagen cargada", use_column_width=True)
         
-        # Button to make predictions
-        if st.button("Make Predictions"):
+        # Button to Realizar predicciones
+        if st.button("Realizar predicciones"):
             # Load and preprocess the uploaded image
-            image = cv2.imread(uploaded_image)
+            im = Image.open(uploaded_image)
+            image = np.asarray(im)
+            image = cv2.resize(image, (64, 64))  # Resize to (64, 64)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Ensure images are in RGB format
             image = image / 255.0  # Normalize pixel values to [0, 1]
-            image = cv2.resize(image, (64, 64))  # Resize to (64, 64)
             image = np.array(image)
 
             # Make a prediction using the loaded model
-            prediction = predictor.predict([image])  # Assuming predictor is a classifier
-            confidence_scores = predictor.predict_proba([image])  # Get confidence scores
+            prediction = predictor.predict(image)  # Assuming predictor is a classifier
 
             # Display the prediction result and confidence scores
             st.write("Prediction:", prediction)
-            st.write("Confidence Scores:", confidence_scores)
 
-elif selection == "Model Performance":
-    st.title("Model Performance")
+elif selection == "Performance de modelos":
+    st.title("Performance de modelos")
 
     # Show performance metrics and interactive graphs
     0
 
-elif selection == "Data Information":
-    st.title("Data Information")
+elif selection == "Información extra":
+    st.title("Información extra")
 
     # Display information about the dataset
     0
